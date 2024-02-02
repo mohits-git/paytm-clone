46def8e94f3d1a6d8ef6bdb45815ae9a180d7ab5
mohits-git <mohitkhatri2805@gmail.com>
2024-02-02 22:04:36 +0530

    another fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom"
+
 function App() {
+   const navigate = useNavigate(); 
+
+    useEffect(() => {
+        navigate('/dashboard')
+    }, [navigate])
 
   return (
     <div>
